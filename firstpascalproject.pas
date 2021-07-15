8158fa9db596d2edf0107eca6d7f275e681c1e31
kakangocthien109 <kakangocthien109@gmail.com>
2021-07-15 14:50:51 +0700

    FP

---
+++ firstpascalproject.pas
@@ -0,0 +1,10 @@
+PROGRAM Pascal;
+USES CRT;
+Begin
+Writeln('***************************************************');
+Writeln('* CHUONG TRINH PASCAL DAU TIEN CUA TOI *');
+Writeln('* Oi! Tuyet voi!... *');
+Writeln('****************************************************');
+Readln;
+End
+.
